61b9894e23278249b5fd0896f50c377f753f5c44
Nuno Ferreira <nunogcferreira@gmail.com>
2024-06-12 01:43:38 +0100

    more keymaps, better descriptions

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -5,12 +5,32 @@
       action = "<cmd>vsplit<CR><C-6>";
       key = "<leader>vs";
       mode = "n";
+      options.desc = "Vertical split";
     }
     # Retain clipboard after pasting visually
     {
       action = "p";
       key = "\"_dp";
       mode = "v";
+    }
+    # Visual stay in indent mode after < or >
+    {
+      action = "<gv";
+      key = "<";
+      mode = "v";
+    }
+    {
+      action = ">gv";
+      key = ">";
+      mode = "v";
+    }
+
+    # stop highlighting with <CR>
+    {
+      action = ":noh<CR><CR>";
+      key = "<CR>";
+      mode = "n";
+      options.noremap = true;
     }
   ];
 }
--- config/plugins/utils/whichkey.nix
+++ config/plugins/utils/whichkey.nix
@@ -2,12 +2,13 @@
   plugins.which-key = {
     enable = true;
     registrations = {
-      "<leader>c" = "+Code";
-      "<leader>f" = "+File finder";
-      "<leader>g" = "+Git";
-      "<leader>n" = "+File tree";
-      "<leader>s" = "+Search";
-      "<leader>u" = "+Utils";
+      "<leader>c" = "Code";
+      "<leader>f" = "File finder";
+      "<leader>g" = "Git";
+      "<leader>n" = "File tree";
+      "<leader>s" = "Search";
+      "<leader>u" = "Utils";
+      "<leader>v" = "Split";
     };
   };
 }
