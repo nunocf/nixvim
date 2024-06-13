175d62ae08445085f9653ca3f810c1a3a7784055
Nuno Ferreira <nunogcferreira@gmail.com>
2024-06-13 17:54:57 +0100

    oil improvements (open in floating window, shows hidden files, etc)

--- config/plugins/utils/oil.nix
+++ config/plugins/utils/oil.nix
@@ -4,35 +4,17 @@
     settings = {
       useDefaultKeymaps = true;
       deleteToTrash = true;
-      float = {
-        padding = 2;
-        maxWidth = 0; # ''math.ceil(vim.o.lines * 0.8 - 4)'';
-        maxHeight = 0; # ''math.ceil(vim.o.columns * 0.8)'';
-        border = "rounded"; # 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-        winOptions = {
-          winblend = 0;
+      skipConfirmForSimpleEdits = false;
+      viewOptions = {
+        showHidden = true;
+        naturalOrder = true;
+        isHiddenFile = {
+          __raw = ''
+            function(name, _)
+              return name == ".." or name == ".git"
+            end
+          '';
         };
-      };
-      preview = {
-        border = "rounded";
-      };
-      keymaps = {
-        "g?" = "actions.show_help";
-        "<CR>" = "actions.select";
-        "<C-\\>" = "actions.select_vsplit";
-        "<C-enter>" = "actions.select_split"; # this is used to navigate left
-        "<C-t>" = "actions.select_tab";
-        "<C-v>" = "actions.preview";
-        "<C-c>" = "actions.close";
-        "<C-r>" = "actions.refresh";
-        "-" = "actions.parent";
-        "_" = "actions.open_cwd";
-        "`" = "actions.cd";
-        "~" = "actions.tcd";
-        "gs" = "actions.change_sort";
-        "gx" = "actions.open_external";
-        "g." = "actions.toggle_hidden";
-        "q" = "actions.close";
       };
     };
   };
@@ -40,7 +22,7 @@
     {
       mode = "n";
       key = "-";
-      action = ":Oil<CR>";
+      action = ":Oil --float<CR>";
       options = {
         desc = "Open parent directory";
         silent = true;
