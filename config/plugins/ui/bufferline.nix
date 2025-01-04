d574f981a11c0fb113357ce9b14aed1fc343223b
Nuno Ferreira <nunogcferreira@gmail.com>
2025-01-04 03:05:50 +0000

    bufferline changes

--- config/plugins/ui/bufferline.nix
+++ config/plugins/ui/bufferline.nix
@@ -12,65 +12,65 @@ in {
         options = {
           separator_style = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
         };
-      };
-      highlights = lib.mkIf config.colorschemes.base16.enable {
-        fill = {
-          fg = colors.base00;
-          bg = colors.base00;
-        };
-        background = {
-          fg = colors.base03;
-          bg = colors.base01;
-        };
-        bufferSelected = {
-          fg = colors.base05;
-          bg = colors.base00;
-          italic = false;
-        };
-        bufferVisible = {
-          fg = colors.base03;
-          bg = colors.base00;
-        };
-        closeButton = {
-          fg = colors.base03;
-          bg = colors.base01;
-        };
-        closeButtonVisible = {
-          fg = colors.base03;
-          bg = colors.base01;
-        };
-        closeButtonSelected = {
-          fg = colors.base08;
-          bg = colors.base00;
-        };
+        highlights = lib.mkIf config.colorschemes.base16.enable {
+          fill = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
+          background = {
+            fg = colors.base03;
+            bg = colors.base01;
+          };
+          buffer_selected = {
+            fg = colors.base05;
+            bg = colors.base00;
+            italic = false;
+          };
+          buffer_visible = {
+            fg = colors.base03;
+            bg = colors.base00;
+          };
+          close_button = {
+            fg = colors.base03;
+            bg = colors.base01;
+          };
+          close_button_visible = {
+            fg = colors.base03;
+            bg = colors.base01;
+          };
+          close_button_selected = {
+            fg = colors.base08;
+            bg = colors.base00;
+          };
 
-        indicatorSelected = {
-          fg = colors.base00;
-          bg = colors.base00;
-        };
-        indicatorVisible = {
-          fg = colors.base00;
-          bg = colors.base00;
-        };
-        separator = {
-          fg = colors.base00;
-          bg = colors.base00;
-        };
-        modified = {
-          fg = colors.base03;
-          bg = colors.base00;
-        };
-        modifiedVisible = {
-          fg = colors.base00;
-          bg = colors.base00;
-        };
-        modifiedSelected = {
-          fg = colors.base0B;
-          bg = colors.base00;
-        };
-        tabClose = {
-          fg = colors.base00;
-          bg = colors.base00;
+          indicator_selected = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
+          indicator_visible = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
+          separator = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
+          modified = {
+            fg = colors.base03;
+            bg = colors.base00;
+          };
+          modified_visible = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
+          modified_selected = {
+            fg = colors.base0B;
+            bg = colors.base00;
+          };
+          tab_close = {
+            fg = colors.base00;
+            bg = colors.base00;
+          };
         };
       };
     };
