cc61e37461d594995331cada336b5558f631925c
Nuno Ferreira <nunogcferreira@gmail.com>
2025-01-04 03:24:56 +0000

    remove liquidsoap support

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  filetype.extension.liq = "liquidsoap";
 
   plugins.treesitter = {
     enable = true;
@@ -15,16 +14,7 @@
     };
 
     folding = true;
-    languageRegister.liq = "liquidsoap";
     nixvimInjections = true;
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
-
-  extraConfigLua = ''
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.liquidsoap = {
-      filetype = "liquidsoap",
-    }
-  '';
 }
